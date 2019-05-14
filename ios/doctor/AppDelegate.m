5ad5e9736194385dc8990010b69b2849e39a1c76
wangbf11 <584623818@qq.com>
2019-05-14 10:15:06 +0800

    安卓和ios 友盟 统计和 3方登录

--- ios/doctor/AppDelegate.m
+++ ios/doctor/AppDelegate.m
@@ -12,10 +12,28 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+//分享
+#import <UMAnalytics/MobClick.h>
+#import <UMShare/UMShare.h>
+#import "RNUMConfigure.h"
+//启动白屏
+#import "RNSplashScreen.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+
+ [UMConfigure setLogEnabled:YES];
+
+  /* 友盟 init */
+  [MobClick setScenarioType:E_UM_NORMAL];
+  [RNUMConfigure initWithAppkey:@"599d6d81c62dca07c5001db6" channel:@"App Store"];
+
+  /* 分享 init */
+  [self setupUSharePlatforms];   // required: setting platforms on demand
+  [self setupUShareSettings];
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"doctor"
@@ -27,8 +45,47 @@
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
+  //启动白屏
+  [RNSplashScreen showSplash:@"LaunchScreen" inRootView:rootView];
   [self.window makeKeyAndVisible];
   return YES;
+}
+- (void)setupUSharePlatforms {
+  /*
+   设置微信的appKey和appSecret
+   [微信平台从U-Share 4/5升级说明]http://dev.umeng.com/social/ios/%E8%BF%9B%E9%98%B6%E6%96%87%E6%A1%A3#1_1
+   */
+  [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_WechatSession appKey:@"wx64e9e5f1b4ecae5e" appSecret:@"a5fbb7fb30a9391cabd8c6d87df4b416" redirectURL:nil];
+  /*
+   * 移除相应平台的分享，如微信收藏
+   */
+  //[[UMSocialManager defaultManager] removePlatformProviderWithPlatformTypes:@[@(UMSocialPlatformType_WechatFavorite)]];
+
+  /* 设置分享到QQ互联的appID
+   * U-Share SDK为了兼容大部分平台命名，统一用appKey和appSecret进行参数设置，而QQ平台仅需将appID作为U-Share的appKey参数传进即可。
+   100424468.no permission of union id
+   [QQ/QZone平台集成说明]http://dev.umeng.com/social/ios/%E8%BF%9B%E9%98%B6%E6%96%87%E6%A1%A3#1_3
+   */
+  [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_QQ appKey:@"101553673"/*设置QQ平台的appID*/  appSecret:nil redirectURL:nil];
+
+}
+
+- (void)setupUShareSettings {
+  /*
+   * 打开图片水印
+   */
+  //[UMSocialGlobal shareInstance].isUsingWaterMark = YES;
+
+  /*
+   * 关闭强制验证https，可允许http图片分享，但需要在info.plist设置安全域名
+   <key>NSAppTransportSecurity</key>
+   <dict>
+   <key>NSAllowsArbitraryLoads</key>
+   <true/>
+   </dict>
+   */
+  [UMSocialGlobal shareInstance].isUsingHttpsWhenShareContent = NO;
+
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
